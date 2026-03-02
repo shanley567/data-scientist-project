91d91edd3cf36259f6937d0b5618389b47d8e253
James Shanley <js10587@hotmail.com>
2026-03-02 17:08:25 +0000

    adding train_test_split

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
@@ -13,8 +14,9 @@ target_col = "CompressiveStrength"
 try: 
 
     X.dropna(subset=[target_col] # Drops rows with missing target values
-            , axis    = 0      # Drop rows (not columns)
-            , inplace = True)  # Changes X directly and not a copy
+            , axis    = 0     # Drop rows (not columns)
+            , inplace = True  # Changes X directly and not a copy
+    )
 
     y = X.pop(target_col)  # Removes target column from X and stores it in y
 
@@ -29,13 +31,21 @@ numerical_cols = [col for col in X.columns
 categorical_cols = [col for col in X.columns 
                     if X[col].dtype == "object"]
 
-feature_cols = numerical_cols + categorical_cols  # Merging col names 
-X            = X[feature_cols]                    # Make sure X only contains the feature columns
+feature_cols = numerical_cols + categorical_cols    # Merging col names 
+X         = X[feature_cols]  # Make sure X only contains the feature columns
+
+# Splitting of data into training and test sets
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, 
+                                                      y, 
+                                                      train_size=0.8, 
+                                                      test_size=0.2,
+                                                      random_state=0)
 
 
   # Preprocessing pipeline
 numerical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant', fill_value=0)),  # Imputes null values = 0
+    ('imputer', SimpleImputer(strategy='constant', fill_value='median')),  # Imputes null values = median
     ('scaler', StandardScaler())                                    # Scales num values to mean=0, stand_dev=1 - used mainly for NNs but does not affect linear regressions 
 ])
 
