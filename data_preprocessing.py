0629c0b9218dc601299668a57c546bad384311b0
James Shanley <js10587@hotmail.com>
2026-03-02 16:37:56 +0000

    adding preprocessing pipeline

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 X_full = pd.read_csv(r"C:\Users\js105\Documents\Coding_portfolio\data-scientist-project\data\concrete.csv")
 
@@ -27,4 +31,24 @@ categorical_cols = [col for col in X.columns
 
 feature_cols = numerical_cols + categorical_cols # Merging col names 
 X = X[feature_cols] # Make sure X only contains the feature columns
+
+
+# Preprocessing pipeline
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value=0)),  # Imputes null values = 0
+    ('scaler', StandardScaler())                                    # Scales num values to mean=0, stand_dev=1 - used mainly for NNs but does not affect linear regressions 
+])
+
+
+categorical_transformer = Pipeline(
+    steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),  # Fills null vals with most frequent vals
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))     # Encodes cardinality columns, ignore unknown categories in test data that were not present in training data
+])
+
+# Batches the transformers and tells to apply to correct columns
+preprocessor = ColumnTransformer([
+    ('num', numerical_transformer, numerical_cols),
+    ('cat', categorical_transformer, categorical_cols)
+])
 
