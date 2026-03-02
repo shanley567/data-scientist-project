b99ad0fa84cff79a55cf0f841ec25a1b32684abd
James Shanley <js10587@hotmail.com>
2026-03-02 16:32:12 +0000

    defining feature columns

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,9 +5,7 @@ X_full = pd.read_csv(r"C:\Users\js105\Documents\Coding_portfolio\data-scientist-
 # seperate target from data
 X = X_full.copy()
 
-
 target_col = "CompressiveStrength"
-
 try:
 
     X.dropna(subset=[target_col] # Drops rows with missing target values
@@ -17,4 +15,16 @@ try:
     y = X.pop(target_col) # Removes target column from X and stores it in y
 
 except KeyError:
-    print(f"Error: Target column NOT found '{target_col}' . \nPossible columns are: {X.columns.tolist()}")
+    print(f"Error: Target column NOT found '{target_col}'. \nPossible columns are: {X.columns.tolist()}")
+
+
+# Defining categorical and numerical columns for feature cols
+numerical_cols = [col for col in X.columns 
+                    if X[col].dtype in ["int64", "float64"]]
+
+categorical_cols = [col for col in X.columns 
+                    if X[col].dtype == "object"]
+
+feature_cols = numerical_cols + categorical_cols # Merging col names 
+X = X[feature_cols] # Make sure X only contains the feature columns
+
